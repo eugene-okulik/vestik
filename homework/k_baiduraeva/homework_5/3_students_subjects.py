560157e14dbe94c6d861065d00d0c82fdde66bb6
Ksenia B <k.baiduraeva@gmail.com>
2026-05-02 11:21:11 +0200

    Homework 5 is done

---
+++ homework/k_baiduraeva/homework_5/3_students_subjects.py
@@ -0,0 +1,7 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+
+subjects = ['math', 'biology', 'geography']
+
+print('Students', ', '.join(students), 'study these subjects:', ', '.join(subjects))
+
+print('Students ' + ', '.join(students) + ' study these subjects: ' + ', '.join(subjects))
