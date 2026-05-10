31d97ce2362c329b987bd1ded298a5fa772e246d
Ksenia B <k.baiduraeva@gmail.com>
2026-05-10 17:02:12 +0200

    homework_6 is edited

--- homework/k_baiduraeva/homework_6/program_fuzz_buzz.py
+++ homework/k_baiduraeva/homework_6/program_fuzz_buzz.py
@@ -1,7 +1,7 @@
-num = range(1, 101)
-
 for num in range(1, 101):
-    if num % 3 == 0:
+    if num % 3 == 0 and num % 5 == 0:
+        print('FuzzBuzz')
+    elif num % 3 == 0:
         print('Fuzz')
     elif num % 5 == 0:
         print('Buzz')
