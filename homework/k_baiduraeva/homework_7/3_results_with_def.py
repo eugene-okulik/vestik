b731d164be87bdab710971eab80818fd4bae60ae
Ksenia B <k.baiduraeva@gmail.com>
2026-05-10 20:36:25 +0200

    Homework 7 is done

--- homework/k_baiduraeva/homework_7/3_results_with_def.py
+++ homework/k_baiduraeva/homework_7/3_results_with_def.py
@@ -3,10 +3,12 @@ line_2 = 'результат операции: 54'
 line_3 = 'результат работы программы: 209'
 line_4 = 'результат: 2'
 
+
 def result(line):
-    #number = int(line[line.index(':') + 1:])
+    # number = int(line[line.index(':') + 1:])
     number = int(line.split(':')[1])
     print(number + 10)
+
 
 result(line_1)
 result(line_2)
